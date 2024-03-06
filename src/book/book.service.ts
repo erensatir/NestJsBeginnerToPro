7f092711ed366cf7ac0fc5f8dc52e954e59df813
Eren Satir <eren.satir@tokeninc.com>
2024-03-06 16:31:05 +0300

    Added Pagination and Search

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -2,16 +2,33 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Book } from './schemas/book.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
+import { Query } from 'express-serve-static-core';
 
 @Injectable()
 export class BookService {
     constructor(
         @InjectModel(Book.name)
         private bookModel: mongoose.Model<Book>
-    ) {}
+    ) { }
 
-    async findAll(): Promise<Book[]> {
-        const books = await this.bookModel.find();
+    async findAll(query: Query): Promise<Book[]> {
+
+        const resPerPage = 2;
+        const currentPage = Number(query.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+        console.log(query);
+
+        const keyword = query.keyword ? {
+            title: {
+                $regex: query.keyword, // Search in the keyword
+                $options: 'i', // Case intensitive
+            }
+        } : {}
+        const books = await this.bookModel
+            .find({ ...keyword })
+            .limit(resPerPage)
+            .skip(skip);
         return books;
     }
 
@@ -23,7 +40,7 @@ export class BookService {
     async findById(id: string): Promise<Book> {
         const book = await this.bookModel.findById(id);
 
-        if(!book) {
+        if (!book) {
             throw new NotFoundException('Book not found')
         }
         return book;
@@ -39,4 +56,4 @@ export class BookService {
     async deleteById(id: string): Promise<Book> {
         return await this.bookModel.findByIdAndDelete(id);
     }
- }
+}
