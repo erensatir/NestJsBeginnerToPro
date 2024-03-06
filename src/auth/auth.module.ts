1e51c2830e666b34478753be60884a1fac96d391
Eren Satir <eren.satir@tokeninc.com>
2024-03-06 20:29:19 +0300

    Added Authorization and updated User related code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    PassportModule.register({ defaultStrategy: 'jwt '}),
+    PassportModule.register({ defaultStrategy: 'jwt'}),
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
@@ -24,6 +25,7 @@ import { ConfigService } from '@nestjs/config';
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema}])
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,5 +1,6 @@
-import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsEmpty, IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 import { Category } from "../schemas/book.schema"
+import { User } from "src/auth/schemas/user.schema";
 
 export class CreateBookDto {
 
@@ -22,4 +23,7 @@ export class CreateBookDto {
     @IsEnum(Category, { message: 'Please enter correct category.' })
     @IsNotEmpty()
     readonly category: Category;
+
+    @IsEmpty({ message: "You can not pass user id" })
+    readonly user: User;
 }
